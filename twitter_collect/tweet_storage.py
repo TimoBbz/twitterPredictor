6e437d1fa769cb9c80b6005fecc7e453d6f70c56
Clémence ALAIN <clemence.alain@student-cs.fr>
2018-11-15 11:42:43 +0100

    fonctions de création de dataframe

--- twitter_collect/tweet_storage.py
+++ twitter_collect/tweet_storage.py
@@ -4,7 +4,7 @@ import unicodedata
 
 def store_tweets(tweets,filename):
      with open(filename,"w") as file:
-        datas={"candidate":filename}
+        datas={}
         for tweet in tweets:
             texte=str(unicodedata.normalize('NFD',tweet.text).encode('ascii','ignore').decode("utf-8"))
             datas[tweet.id]={"text":texte,"date":str(tweet.created_at)}
