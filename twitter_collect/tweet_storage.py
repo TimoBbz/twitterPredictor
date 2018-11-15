5f3928aec5d3976a4aead3c92eed1c54e3e5842f
Clémence ALAIN <clemence.alain@student-cs.fr>
2018-11-15 11:22:39 +0100

    finalisation fct store tweets

--- twitter_collect/tweet_storage.py
+++ twitter_collect/tweet_storage.py
@@ -1,11 +1,11 @@
 import json
+import unicodedata
 
 
 def store_tweets(tweets,filename):
      with open(filename,"w") as file:
-        datas={}
+        datas={"candidate":filename}
         for tweet in tweets:
-            datas["id"]=tweet.id
-            datas["id"]["text"]=tweet.text
-            datas["id"]["date"]=tweet.created_at
+            texte=str(unicodedata.normalize('NFD',tweet.text).encode('ascii','ignore').decode("utf-8"))
+            datas[tweet.id]={"text":texte,"date":str(tweet.created_at)}
         json.dump(datas,file)
