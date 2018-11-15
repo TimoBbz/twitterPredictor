24c7f8dd49c0305b1cc5c78102d6fdbe5b8c873b
Timothé Bailly-Barthez <timothe.barthez@student-cs.fr>
2018-11-15 11:06:31 +0100

    ajout du fichier __main__.py et correction de twitter_get.py

--- twitter_collect/twitter_get.py
+++ twitter_collect/twitter_get.py
@@ -1,16 +1,6 @@
 from twitter_collect import twitter_connection_setup as tcs
 
 
-def collect():
+def collect(keyword):
     connexion = tcs.twitter_setup()
-    tweets = connexion.search("Emmanuel Macron",language="french",rpp=100)
-    for tweet in tweets:
-        print(tweet.text)
-
-
-def collect_by_user(user_id):
-    connexion = tcs.twitter_setup()
-    statuses = connexion.user_timeline(id = user_id, count = 200)
-    for status in statuses:
-        print(status.text)
-    return
+    return connexion.search(keyword,language="french",rpp=100)
