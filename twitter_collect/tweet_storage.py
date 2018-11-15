c1aca5ab222ece6a130aa90a6e3af905a1d8b2a6
Clémence ALAIN <clemence.alain@student-cs.fr>
2018-11-15 10:19:59 +0100

    mineures modifs de la fonction de stockage

--- twitter_collect/tweet_storage.py
+++ twitter_collect/tweet_storage.py
@@ -3,9 +3,9 @@ import json
 
 def store_tweets(tweets,filename):
      with open(filename,"w") as file:
-         datas={}
-         for tweet in tweets:
-             datas["id"]=tweet.id
-             datas["id"]["text"]=tweet.text
-             datas["id"]["data"]=tweet.created_at
+        datas={}
+        for tweet in tweets:
+            datas["id"]=tweet.id
+            datas["id"]["text"]=tweet.text
+            datas["id"]["date"]=tweet.created_at
         json.dump(datas,file)
