8dc894bcaaa78c0cebdc72a2e1478ed2809a5300
Timothé Bailly-Barthez <timothe.barthez@student-cs.fr>
2018-11-15 11:29:36 +0100

    finalisation fct main module tweet_collect

--- twitter_collect/__main__.py
+++ twitter_collect/__main__.py
@@ -5,18 +5,17 @@ import tweepy
 
 with open("../CandidateData/candidates.txt") as candidates:
     names_str = candidates.read() # récupération des candidats dans la variable name
-print(names_str)
 names_dict = literal_eval(names_str)
 for id in names_dict:
     tweet_global = tweepy.models.SearchResults()
     #récupération des tweets par mot-clé
-    with open ("../CandidateData/keywords_candidate_" + str(id) + ".txt") as kw:
+    with open("../CandidateData/keywords_candidate_" + str(id) + ".txt") as kw:
         kw_str = kw.read()
     kw_set = literal_eval(kw_str)
     for kw in kw_set:
         tweet_global += tg.collect(kw)
     #récupération des tweets par mot_dièse
-    with open ("../CandidateData/hashtag_candidate_" + str(id) + ".txt") as ht:
+    with open("../CandidateData/hashtag_candidate_" + str(id) + ".txt") as ht:
         ht_str = ht.read()
     ht_set = literal_eval(ht_str)
     for ht in ht_set:
