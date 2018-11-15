d764793a407ba87eb36f9108512bab4b5635b10b
Clémence ALAIN <clemence.alain@student-cs.fr>
2018-11-15 11:47:24 +0100

    fonction dataframe_setup working

--- twitter_analysis/dataframe_setup.py
+++ twitter_analysis/dataframe_setup.py
@@ -1,12 +1,11 @@
 import pandas
 import json
 
-def get_tweets(filename):
+
+def set_dataframe(filename):
+    '''tweets is a json object containing all tweets about a candidate'''
     tweets=[]
     with open(filename,"r",encoding="utf-8") as fichier:
-        tweets=json.loads(fichier)
-    print(tweets)
-
-def set_dataframe(tweets):
-    '''tweets is a json object containing all tweets about a candidate'''
-
+        tweets=json.load(fichier)
+    df2=pandas.DataFrame(tweets)
+    print(df2)
