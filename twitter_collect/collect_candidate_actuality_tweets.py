0dc953258666480b632efde47c20e2c400bb1707
Timothé Bailly-Barthez <timothe.barthez@student-cs.fr>
2018-11-15 09:56:43 +0100

    ajout de la fonction de streaming

--- twitter_collect/collect_candidate_actuality_tweets.py
+++ twitter_collect/collect_candidate_actuality_tweets.py
@@ -1,3 +1,9 @@
+import tweepy
+from tweepy import StreamListener
+from tweepy.streaming import StreamListener
+from twitter_collect import twitter_connection_setup as tcs
+
+
 def get_tweets_from_candidates_search_queries(queries, twitter_api):
     tweet_list = []
     for query in queries:
@@ -5,3 +11,30 @@ def get_tweets_from_candidates_search_queries(queries, twitter_api):
         for tweet in tweets:
             tweet_list.append(tweet.text)
     return tweet_list
+
+
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        if str(status) == "420":
+            print(status)
+            print("You exceed a limited number of attempts to connect to the streaming API")
+            return False
+        else:
+            return True
+
+
+def collect_by_streaming(num_candidate):
+    connexion = tcs.twitter_setup()
+    #création de l'instance de connexion
+    listener = StdOutListener()
+
+    stream = tweepy.Stream(auth=connexion.auth, listener=listener)
+    stream.filter(follow=[num_candidate], track=["Emmanuel Macron"])
+    stream.filter()
+
+
