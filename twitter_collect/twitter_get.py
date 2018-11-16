c5a501858ef774b2a451f78323e9184a0cc91996
Clémence ALAIN <clemence.alain@student-cs.fr>
2018-11-16 15:22:04 +0100

    augmentation de la qtt de tweets récupérés

--- twitter_collect/twitter_get.py
+++ twitter_collect/twitter_get.py
@@ -1,6 +1,14 @@
 from twitter_collect import twitter_connection_setup as tcs
-
+import tweepy
 
 def collect(keyword):
     connexion = tcs.twitter_setup()
-    return connexion.search(keyword,language="french")
+    first_id=1063189466007969792
+    searches = tweepy.models.SearchResults()
+    while True:
+        new_searches=connexion.search(keyword,language="french",count=100,since_id=first_id)
+        first_id=new_searches[len(new_searches)-1].id
+        searches+=new_searches
+        if len(new_searches)==0 or len(searches)>1000:
+            break;
+    return searches
